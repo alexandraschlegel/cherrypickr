93980d402dda8d8916ba50593a3c3f9716a2c68c
Alexandra Schlegel <alexandra.schlegel@yahoo.de>
2020-03-03 11:08:13 +0000

    models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_105837) do
+ActiveRecord::Schema.define(version: 2020_03_03_110728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.bigint "supermarkets_id", null: false
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_deals_on_products_id"
+    t.index ["supermarkets_id"], name: "index_deals_on_supermarkets_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "supermarkets_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["supermarkets_id"], name: "index_products_on_supermarkets_id"
+  end
+
+  create_table "supermarkets", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_products", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_user_products_on_products_id"
+    t.index ["users_id"], name: "index_user_products_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +57,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_105837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "products", column: "products_id"
+  add_foreign_key "deals", "supermarkets", column: "supermarkets_id"
+  add_foreign_key "products", "supermarkets", column: "supermarkets_id"
+  add_foreign_key "user_products", "products", column: "products_id"
+  add_foreign_key "user_products", "users", column: "users_id"
 end
