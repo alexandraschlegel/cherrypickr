76ba82325b08d5feaa3068e0b09f2adb4f118404
Naomi Arefaine <naomiarefaine@gmail.com>
2020-03-04 10:29:19 +0000

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require "open-uri"
 require "nokogiri"
 
+Product.destroy_all
+Supermarket.destroy_all
 Supermarket.create()
 
-url = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all"
-doc = Nokogiri::HTML(open(url))
-doc.search(".tile-content").each do |result_card|
-  name = result_card.search(".sc-cSHVUG").text
-  price = result_card.search(".price-per-sellable-unit").text
-  Product.create!(name: name, price_cents: price.split.last.to_f, supermarket_id: Supermarket.first.id)
+base_url = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all"
+
+10.times do |i|
+  page_number = i + 1
+  page = "?page=#{page_number}"
+  url = base_url + page + "&count=48"
+  doc = Nokogiri::HTML(open(url))
+  doc.search(".tile-content").each do |result_card|
+    name = result_card.search(".sc-cSHVUG").text
+    price = result_card.search(".price-per-sellable-unit").text
+    Product.create!(name: name, price_cents: price.split.last.to_f, supermarket_id: Supermarket.first.id)
+  end
 end
