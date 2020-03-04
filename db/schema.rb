295f1157789b59bdbf474b53aac7bd8220ed9fa6
Eric Adu <eric.adu@btinternet.com>
2020-03-04 09:57:54 +0000

    adds fields to supermarket & product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_113354) do
+ActiveRecord::Schema.define(version: 2020_03_03_141852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 2020_03_03_113354) do
   create_table "supermarkets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "name"
   end
 
   create_table "user_products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,37 @@ doc.search(".tile-content").each do |result_card|
   price = result_card.search(".price-per-sellable-unit").text
   Product.create!(name: name, price_cents: price.split.last.to_f, supermarket_id: Supermarket.first.id)
 end
+
+#Users
+#User.new(email: 'eric@gmail.com', password: '123456')
+#User.new(email: 'priyesh@talktalk.com', password: '123456')
+#User.new(email: 'nimbala@cloudnet.com', password: '123456')
+#
+#Supermarkets
+#Supermarket.new(name: 'Tesco Wembley', address:)
+#Supermarket.new(name: 'Asda Wembley', address: )
+#Supermarket.new(name: 'Sainsbury Kenton', address: )
+#Supermarket.new(name: 'Sainsbury Alperton', address: )
+#Supermarket.new(name: 'Tesco Perivale', address:)
+
+
+#Products
+#Product.new(name: "Fairtrade Bananas" price_cents: 100, weight: 1, description:"Fairtrade Bananas" )
+#Product.new(name: "Cherry Vine Tomatoes" price_cents: 100, weight: 1, description:"Fairtrade Bananas" )
+#Product.new(name: "Fairtrade Bananas" price_cents: 100, weight: 1, description:"Fairtrade Bananas" )
+#Product.new(name: "Fairtrade Bananas" price_cents: 100, weight: 1, description:"Fairtrade Bananas" )
+
+#user_products
+#User_products.new(user_id: 1, product_id: 1)
+#User_products.new(user_id: 1, product_id: 2)
+#User_products.new(user_id: 2, product_id: 1)
+#User_products.new(user_id: 2, product_id: 2)
+
+
+#deals
+#Deals.new(supermarket_id: 1,  product_id: 1)
+#Deals.new(supermarket_id: 1,  product_id: 2)
+#Deals.new(supermarket_id: 1,  product_id: 3)
+#Deals.new(supermarket_id: 2,  product_id: 1)
+#Deals.new(supermarket_id: 2,  product_id: 2)
+#Deals.new(supermarket_id: 2,  product_id: 3)
