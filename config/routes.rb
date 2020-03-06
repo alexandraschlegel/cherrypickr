f329423a6ad52d924020fa437ace63ddd9f10404
alexander haumer <alexander.haumer@me.com>
2020-03-06 12:11:13 +0000

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+
   resources :users, only: [:show]
 
   resources :products, only: [:index, :show] do
     resources :user_products, only: [:create, :new]
 
   end
+  resources :user_products
 end
