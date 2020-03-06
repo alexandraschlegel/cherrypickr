53547578e34150f540cad595997e8ecb972dc2b6
Alexandra Schlegel <alexandra.schlegel@yahoo.de>
2020-03-06 17:00:42 +0000

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :user_products
+  has_many :user_products, dependent: :destroy
   has_many :products, through: :user_products
 end
