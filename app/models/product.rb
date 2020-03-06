63279f9e88958dfd5f9a655fb06e0d7d232be9ec
Eric Adu <eric.adu@btinternet.com>
2020-03-06 10:43:01 +0000

    Corrected errors in models and added geocoder.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :deals
   belongs_to :supermarket
 
-    include PgSearch::Model
+  include PgSearch::Model
   pg_search_scope :search_by_name,
     against: :name,
     using: {
--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -2,6 +2,14 @@ class Supermarket < ApplicationRecord
   has_many :products
   has_many :deals
 
-  geocoded_by :address
+  geocoded_by :postcode
   after_validation :geocode
+
+
+  def postcode
+    #only execute if an eddress available else it throws exception
+    if !address.nil?
+      address.split(",").last
+    end
+  end
 end
